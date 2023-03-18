8f5d33901c23cc3005712b0a1d2089fb0423503d
webmanana <lukrs@mail.ru>
2023-03-18 17:01:41 +0300

    12312

--- dist/dashboard4.js
+++ dist/dashboard4.js
@@ -189,73 +189,7 @@ $(function () {
   // -----------------------------------------------------------------------
   // world map
   // -----------------------------------------------------------------------
-  jQuery("#visitfromworld").vectorMap({
-    map: "world_mill_en",
-    backgroundColor: "#fff",
-    borderColor: "#ccc",
-    borderOpacity: 0.9,
-    borderWidth: 1,
-    zoomOnScroll: false,
-    color: "#ddd",
-    regionStyle: {
-      initial: {
-        fill: "rgba(0,0,0,0.1)",
-      },
-    },
-    markerStyle: {
-      initial: {
-        r: 8,
-        fill: "#55ce63",
-        "fill-opacity": 1,
-        stroke: "#000",
-        "stroke-width": 0,
-        "stroke-opacity": 1,
-      },
-    },
-    enableZoom: true,
-    hoverColor: "#79e580",
-    markers: [
-      {
-        latLng: [21.0, 78.0],
-        name: "India : 9347",
-        style: { fill: "#55ce63" },
-      },
-      {
-        latLng: [-33.0, 151.0],
-        name: "Australia : 250",
-        style: { fill: "#02b0c3" },
-      },
-      {
-        latLng: [36.77, -119.41],
-        name: "USA : 250",
-        style: { fill: "#11a0f8" },
-      },
-      {
-        latLng: [55.37, -3.41],
-        name: "UK   : 250",
-        style: { fill: "#745af2" },
-      },
-      {
-        latLng: [25.2, 55.27],
-        name: "UAE : 250",
-        style: { fill: "#ffbc34" },
-      },
-    ],
-    hoverOpacity: null,
-    normalizeFunction: "linear",
-    scaleColors: ["#fff", "#ccc"],
-    selectedColor: "#c9dfaf",
-    selectedRegions: [],
-    showTooltip: true,
-    onRegionClick: function (element, code, region) {
-      var message =
-        'You clicked "' +
-        region +
-        '" which has the code: ' +
-        code.toUpperCase();
-      alert(message);
-    },
-  });
+ 
   $("#calendar").fullCalendar("option", "height", 590);
   // -----------------------------------------------------------------------
   // sparkline chart
